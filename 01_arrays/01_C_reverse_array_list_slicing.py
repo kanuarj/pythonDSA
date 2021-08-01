69cfa83c9e6db96555420d7c52dbaba46dde1b4b
kanuarj <jimmy.lpf@gmail.com>
2021-08-01 20:27:53 +0530

    List Slicing Reverse Array

---
+++ 01_arrays/01_C_reverse_array_list_slicing.py
@@ -0,0 +1,7 @@
+def list_slicing_reverse_array(input_array):
+    return input_array[::-1]
+
+if __name__ == '__main__':
+    input_array = [1,2,3,4,5,6,7,8,9]
+    output_array = list_slicing_reverse_array(input_array)
+    print(output_array)
