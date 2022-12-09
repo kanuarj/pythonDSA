100e5418b10b261f841053ad662ff200ce3e8fdb
kanuarj <jimmy.lpf@gmail.com>
2022-12-09 20:19:38 +0530

    Optimized Swapping Nodes for Linked List

--- LinkedList/singlyLinkedList/18_swapNodesInLLwithoutSwappingData.py
+++ LinkedList/singlyLinkedList/18_swapNodesInLLwithoutSwappingData.py
@@ -1,3 +1,20 @@
+# Swap nodes in a linked list without swapping data
+
+# Given a linked list and two keys in it, swap nodes for two given keys.
+# Nodes should be swapped by changing links.
+# Swapping data of nodes may be expensive in many situations when data contains many fields.
+# It may be assumed that all keys in the linked list are distinct.
+
+# Examples: 
+# Input: 10->15->12->13->20->14,  x = 12, y = 20
+# Output: 10->15->20->13->12->14
+
+# Input: 10->15->12->13->20->14,  x = 10, y = 20
+# Output: 20->15->12->13->10->14
+
+# Input: 10->15->12->13->20->14,  x = 12, y = 13
+# Output: 10->15->13->12->20->14
+
 class Node:
     def __init__ (self, data):
         self.data = data
@@ -61,13 +78,36 @@ class LinkedList:
 
     # Time Complexity: O(N)
     # Auxiliary Space: O(1) 
+    # ====================================================================================
+    # Optimized solution using single traversal
+    def swapNodesOptimized (self, x, y):
+        if x == y:
+            return None
+        temp = self.head
+        first = second = None
+        while temp.next is not None:
+            if temp.next.data == x:
+                first = temp
+            elif temp.next.data == y:
+                second = temp
+            temp = temp.next
 
+        if first is not None and second is not None:
+            temp = first.next
+            first.next = second.next
+            second.next = temp
+            temp = first.next.next
+            first.next.next = second.next.next
+            second.next.next = temp
     
+    # Time complexity: O(N)
+    # Auxiliary Space: O(1)
+
 if __name__ == '__main__':
     ll = LinkedList ()
     ll.insertInEnd (1)
     ll.insertInEnd (2)
     ll.insertInEnd (3)
     ll.traversal ()
-    ll.swapNodes (2, 3)
+    ll.swapNodesOptimized (2, 3)
     ll.traversal ()
