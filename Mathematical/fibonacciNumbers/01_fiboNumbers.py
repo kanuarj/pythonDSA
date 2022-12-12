59b2c913dc9cce086a2bb846c2ba72457d61e3f0
kanuarj <jimmy.lpf@gmail.com>
2022-12-12 20:45:04 +0530

    Fibo Space Optimized Method

--- Mathematical/fibonacciNumbers/01_fiboNumbers.py
+++ Mathematical/fibonacciNumbers/01_fiboNumbers.py
@@ -13,8 +13,27 @@ def fiboDynamic (n):
     return holder[n]
 # Time complexity: O(n) for given n
 # Auxiliary space: O(n)
+# ===========================================================================================
+# Space Optimized Method
+def fiboSpaceOptimized (n):
+    a, b = 0, 1
+    if n < 0:
+        return -1
+    elif n == 0:
+        return a
+    elif n == 1:
+        return b
+    else:
+        for i in range (2, n+1):
+            c = a + b
+            a = b
+            b = c
+        return b
+# Time Complexity: O(n) 
+# Extra Space: O(1)
 
 if __name__ == '__main__':
     n = 9
     # print (fiboRecursive (n))
-    print (fiboDynamic(n))
+    # print (fiboDynamic(n))
+    print (fiboSpaceOptimized (n))
