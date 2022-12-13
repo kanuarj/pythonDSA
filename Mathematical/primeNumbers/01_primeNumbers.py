d1bba7f4aff8dabff629508cfd291385ba97211a
kanuarj <jimmy.lpf@gmail.com>
2022-12-13 20:23:03 +0530

    Prime Numbers using Efficient Approach

--- Mathematical/primeNumbers/01_primeNumbers.py
+++ Mathematical/primeNumbers/01_primeNumbers.py
@@ -15,6 +15,27 @@ def isPrimeNaiveApproach (n):
 
 # Time Complexity: O(sqrt(n))
 # Auxiliary space: O(1)
+# =================================================================================================================================
+# Efficient Approach 
+def isPrimeEfficientApproach (n):
+    if n <= 1:
+        return False
+    
+    if n == 2 or n == 3:
+        return True
+
+    if n % 2 is 0 or n % 3 == 0:
+        return False
+
+    for i in range (5, int(sqrt(n)), 6):
+        if n % i is 0 or n % (i+2) is 0:
+            return False
+
+    return True
+
+# Time complexity: O(sqrt(n))
+# Auxiliary space: O(1)
 
 if __name__ == '__main__':
-    print (isPrimeNaiveApproach (4))
+    # print (isPrimeNaiveApproach (4))
+    print (isPrimeEfficientApproach (11))
