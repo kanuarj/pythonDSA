27fc4c49ed75064b92b27b3b9c6ccfabbb45845d
kanuarj <jimmy.lpf@gmail.com>
2022-12-13 22:18:06 +0530

    Factorial of a Number

--- Mathematical/factorial/01_factorialOfNumber.py
+++ Mathematical/factorial/01_factorialOfNumber.py
@@ -7,5 +7,21 @@ def factorialRecursive (n):
 # Time Complexity: O(n)
 # Auxiliary Space: O(n)
 
+def factorialAnotherApproach (n):
+    if n is 0:
+        return 1
+    
+    i, factorial = n, 1
+
+    while (n/i) != n:
+        factorial *= i
+        i -= 1
+
+    return factorial
+
+# Time complexity: O(N)
+# Auxiliary Space: O(1)
+
 if __name__ == '__main__':
-    print (factorialRecursive (5))
+    # print (factorialRecursive (5))
+    print (factorialAnotherApproach (5))
