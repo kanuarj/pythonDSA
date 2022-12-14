a14ee1a3526afdedf290df1c515ee01011c05f97
kanuarj <jimmy.lpf@gmail.com>
2022-12-14 21:01:01 +0530

    Power of 3 Recursive Function

--- Mathematical/divisibility/03_powerOf3IntChecking.py
+++ Mathematical/divisibility/03_powerOf3IntChecking.py
@@ -16,6 +16,23 @@ def generalSolution (n):
 
 # Time Complexity: O(1)
 # Space Complexity: O(1)
+# ========================================================
+# Recursive Solution 
+def recursiveSolution (n):
+    if n <= 0:
+        return False
+
+    if n is 1:
+        return True
+
+    if n % 3 is 0:
+        return recursiveSolution (n // 3)
+
+    return False
+
+# Time Complexity: O(log3n), where n represents the given integer.
+# Auxiliary Space: O(log3n).
 
 if __name__ == '__main__':
-    print (generalSolution (16))
+    # print (generalSolution (16))
+    print (recursiveSolution (9))
