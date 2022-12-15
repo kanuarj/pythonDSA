3176dbc250adde0c8e9d7ef04f1a03612fd3976f
kanuarj <jimmy.lpf@gmail.com>
2022-12-15 21:24:21 +0530

    Move Zeroes using Partitioning

--- Arrays/standardProblems/Easy/02_moveZeroesToEnd.py
+++ Arrays/standardProblems/Easy/02_moveZeroesToEnd.py
@@ -23,8 +23,24 @@ def pushZeroes (arr, n, count = 0):
 
 # Time Complexity: O(n) where n is the size of elements of the input array.
 # Auxiliary Space: O(1)
+# ============================================================================================================
+# Partitioning the array approach :
+# The approach is pretty simple. We will use 0 as a pivot element and whenever we see a non zero element we will swap it with the pivot element. So all the non zero element will come at the beginning.
+# The approach is pretty simple.
+# We will use 0 as a pivot element and whenever we see a non zero element we will swap it with the pivot element.
+# So all the non zero element will come at the beginning.
+
+def partitioningProcedure (arr, n, j = 0):
+    for i in range (n):
+        if arr[i] is not 0:
+            arr[j], arr[i] = arr[i], arr[j]
+            j += 1
+
+# Time Complexity: O(N), where N is the size of elements of the input array.
+# Auxiliary Space: O(1)
 
 if __name__ == '__main__':
     arr = [1, 9, 8, 4, 0, 0, 2, 7, 0, 6, 0, 9]
-    pushZeroes (arr, len (arr))
+    # pushZeroes (arr, len (arr))
+    partitioningProcedure (arr, len (arr))
     print (arr)
