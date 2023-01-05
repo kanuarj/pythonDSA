c0fa77979704d8524c0d68a514a6b328a65a0655
kanuarj <jimmy.lpf@gmail.com>
2023-01-05 17:13:27 +0530

    LCS DP

--- dynamicProgramming/02_longestCommonSubsequence.py
+++ dynamicProgramming/02_longestCommonSubsequence.py
@@ -18,9 +18,24 @@ def memoizationLCS (X, Y, m, n, memoi):
     memoi[m][n] = max (memoizationLCS (X, Y, m-1, n, memoi), memoizationLCS (X, Y, m, n-1, memoi))
     return memoi[m][n]
 
+def LCSDP (X, Y):
+    m, n = len (X), len (Y)
+    L = [[None]*(n+1) for _ in range (m+1)]
+    for i in range (m+1):
+        for j in range (n+1):
+            if i == 0 or j == 0:
+                L[i][j] = 0
+            elif X[i-1] == Y[j-1]:
+                L[i][j] = 1 + L[i-1][j-1]
+            else:
+                L[i][j] = max (L[i-1][j], L[i][j-1])
+
+    return L[m][n]            
+
 if __name__ == '__main__':
     X = 'CGATAATTGAGA'
     Y = 'GTTCCTAATA'
     memoi = [[-1 for _ in range(len(Y)+1)] for _ in range(len(X)+1)]
     # print (recursiveLCS (X, Y, len(X), len(Y)))
-    print (memoizationLCS (X, Y, len(X), len(Y), memoi))
+    # print (memoizationLCS (X, Y, len(X), len(Y), memoi))
+    print (LCSDP (X, Y))
